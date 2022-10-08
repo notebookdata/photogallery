96e3517a9e84c4438fa9b0f936f06408a904c86a
Mallappa <mbsanabhangi@outlook.com>
2022-10-08 20:28:45 +0530

    Added basic version of project

---
+++ photo-gallery-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/WorkSpace/django-photo-gallery/django_photo_gallery/photo-gallery-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
